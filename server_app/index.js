9c3d834997b51b20893be1f26707c350699c0473
kaito2001 <kaito.ptit@gmail.com>
2023-11-28 09:37:44 +0700

    update admin site | status: done

--- server_app/index.js
+++ server_app/index.js
@@ -44,25 +44,9 @@ app.use(cors());
 
 app.use('/api/User', UserAPI)
 
-app.use('/api/admin/Product', ProductAdmin)
-app.use('/api/admin/Category', CategoryAdmin)
 app.use('/api/admin/Permission', Permission)
 app.use('/api/admin/User', UserAdmin)
-app.use('/api/admin/Order', Order)
-app.use('/api/admin/Coupon', Coupon)
-app.use('/api/admin/Sale', Sale)
 
-
-io.on("connection", (socket) => {
-  console.log(`Có người vừa kết nối, socketID: ${socket.id}`);
-
-
-  socket.on('send_order', (data) => {
-    console.log(data)
-
-    socket.broadcast.emit("receive_order", data);
-  })
-})
 
 http.listen(port, () => {
   console.log('listening on *: ' + port);
